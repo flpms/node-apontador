79d9d035512c7cbf14c567d36b71bfb8d85870c8
flpms <filipemelodasilva@gmail.com>
2017-05-06 15:37:44 -0300

    create really unit test

---
+++ test/unit/lib/response-handlers.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const sinon = require('sinon');
+
+const responseHandlers = require('response-handlers.js');
+const EventEmitter = require('events');
+
+describe('Expect handler with events when delivery response with correct status', function() {
+    it('Should call `data` event after event emitted', function() {
+        
+    });
+}):
