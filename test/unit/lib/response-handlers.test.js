ad0acf7a12b99ac11ce7fc8bdb3f7c49bfcd6562
flpms <filipemelodasilva@gmail.com>
2017-05-06 23:44:00 -0300

    write unit test file

--- test/unit/lib/response-handlers.test.js
+++ test/unit/lib/response-handlers.test.js
@@ -1,12 +1,42 @@
 'use strict';
 
-const sinon = require('sinon');
+process.env.NODE_ENV = 'test'
 
-const responseHandlers = require('response-handlers.js');
-const EventEmitter = require('events');
+const chai = require('chai');
+const sinon = require('sinon');
+const EventEmitter = require('events').EventEmitter;
+
+const handler = require('../../../lib/response-handlers.js');
+
+const expect = chai.expect;
+const res = new EventEmitter();
+
+const responseHandlers = handler.responseHandlers;
+const dataHandlers = handler.dataHandlers;
 
 describe('Expect handler with events when delivery response with correct status', function() {
-    it('Should call `data` event after event emitted', function() {
-        
+
+    it('expect call callback after `data` event is emitted', function() {
+
+        var data = {
+            callback: function() {}
+        };
+
+        var spy = sinon.spy(data, 'callback');
+        var expectArgs = '{ "code": 200, "results" : []}';
+
+        dataHandlers(data, res);
+
+        res.emit('data', expectArgs);
+        res.emit('end');
+
+        expect(spy.calledOnce).to.be.true;
     });
-}):
+
+    it('expect string parsed to json after call `end` and responseHandlers', function() {
+
+        responseHandlers();
+
+    })
+
+})
